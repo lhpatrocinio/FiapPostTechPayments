9f31d635873216ae7f2f3e620c37ead0f115f404
Marcelo Morais <marceloms17@gmail.com>
2025-09-29 17:04:21 -0300

    Feat: Logstash, Serilog

--- src/Payments.Api/Program.cs
+++ src/Payments.Api/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Asp.Versioning.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using Payments.Api.Extensions.Auth;
 using Payments.Api.Extensions.Auth.Middleware;
 using Payments.Api.Extensions.Logs;
+using Payments.Api.Extensions.Logs.ELK;
 using Payments.Api.Extensions.Logs.Extension;
 using Payments.Api.Extensions.Mappers;
 using Payments.Api.Extensions.Migration;
@@ -38,12 +40,13 @@ builder.Services.AddCors(options =>
     });
 });
 
-//// Adiciona monitoramento com Prometheus
-//builder.Services.AddPrometheusMonitoring();
-//builder.Services.AddMetricsCollector();
+// Adiciona monitoramento com Prometheus
+builder.Services.AddPrometheusMonitoring();
 
+// Adiciona integração ELK
+builder.Services.AddELKIntegration(builder.Configuration);
 
-#region [DI]]
+#region [DI]
 
 ApplicationBootstrapper.Register(builder.Services);
 InfraBootstrapper.Register(builder.Services);
@@ -55,19 +58,21 @@ var app = builder.Build();
 app.ExecuteMigrations();
 var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 
-app.UseAuthentication();                        // 1�: popula HttpContext.User
-app.UseMiddleware<RoleAuthorizationMiddleware>(); // 2�: seu middleware
+app.UseAuthentication();                        // 1°: popula HttpContext.User
+app.UseMiddleware<RoleAuthorizationMiddleware>(); // 2°: seu middleware
 app.UseCorrelationId();
+app.UseELKIntegration();
 
 // Adiciona CORS antes de outros middlewares
 app.UseCors("AllowAll");
 
-//// Adiciona middleware de monitoramento
-//app.UsePrometheusMonitoring();
-//app.UseMetricsMiddleware();
+// Adiciona middleware de monitoramento
+app.UsePrometheusMonitoring();
+
+// Adiciona request logging com Serilog
+app.UseSerilogRequestLogging();
 
 app.UseVersionedSwagger(apiVersionDescriptionProvider);
-app.UseAuthorization();                         // 3�: aplica [Authorize]
-app.UseHttpsRedirection();
+app.UseAuthorization();                         // 3°: aplica [Authorize]
 app.MapControllers();
 app.Run();
